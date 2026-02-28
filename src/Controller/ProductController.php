3b14fcf6f5419c17b790d3a5a4465c6cbc072150
cmouns <mounir.sebti33@email.com>
2026-03-01 00:33:37 +0100

    feat(front): integrate accessible and responsive product pages

--- src/Controller/ProductController.php
+++ src/Controller/ProductController.php
@@ -2,6 +2,7 @@
 
 namespace App\Controller;
 
+use App\Entity\Product;
 use App\Repository\ProductRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
@@ -17,4 +18,20 @@ final class ProductController extends AbstractController
             'products' => $products ,
         ]);
     }
+
+    #[Route('/product/{slug}', name: 'app_product_show')]
+    public function showone(string $slug, ProductRepository $productRepository): Response
+    {
+        $product = $productRepository->findOneBy(['slug' => $slug]);
+
+        if (!$product) {
+            throw $this->createNotFoundException('Ce produit n\'existe pas.');
+        }
+
+        return $this->render('product/show.html.twig', [
+            'product' => $product,
+        ]);
+    }
+
+
 }
